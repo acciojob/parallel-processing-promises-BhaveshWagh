eb554ea6240064899fc59e9d0cfab6054c6db8d6
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-07-24 11:35:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +7,36 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+  const promises = images.map((image) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+      img.src = image.url;
+    });
+  });
+
+  Promise.all(promises)
+   .then((images) => {
+      images.forEach((img) => {
+        output.appendChild(img);
+      });
+    })
+   .catch((error) => {
+      console.error(error);
+    });
+});
+// const output = document.getElementById("output");
+// const btn = document.getElementById("download-images-button");
+
+// const images = [
+//   { url: "https://picsum.photos/id/237/200/300" },
+//   { url: "https://picsum.photos/id/238/200/300" },
+//   { url: "https://picsum.photos/id/239/200/300" },
+// ];
